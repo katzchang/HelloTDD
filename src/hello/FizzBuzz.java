4a72288da0f2d5ba334a5474283a82da0478670b
The Author <The.author@some.com>
2010-11-01 03:05:01 +0900

    FizzBuzzTest:OK, total:4, pass:4, fail:0, error:0, ignore:0
    TestCase: hello.FizzBuzzTest._3のときはFizz : Completed - OK
    TestCase: hello.FizzBuzzTest._5のときはBuzz : Completed - OK
    TestCase: hello.FizzBuzzTest._15のときはFizzBuzz : Completed - OK
    TestCase: hello.FizzBuzzTest._1のときは1 : Completed - OK

--- src/hello/FizzBuzz.java
+++ src/hello/FizzBuzz.java
@@ -6,7 +6,7 @@ public class FizzBuzz {
 		if (i % 15 == 0) return "FizzBuzz";
 		if (i % 3 == 0) return "Fizz";
 		if (i % 5 == 0) return "Buzz";
-		return null;
+		return String.valueOf(i);
 	}
 
 }
